bd667e6be209bdaaa78a9296dbe8ec61370337a2
LinaHai1 <Lina.Haigoun@emsi-edu.ma>
2025-05-02 04:44:21 +0100

    Update goodbye.js

--- goodbye.js
+++ goodbye.js
@@ -1,9 +1,10 @@
 (function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
+  var speakWord = "Good Bye";
+  var byeSpeaker = {
+    speak: function(name) {
+      console.log(speakWord + " " + name);
+    }
+  };
 
+  window.byeSpeaker = byeSpeaker;
 })(window);

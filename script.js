76eb1bed081feec2015dd91e8832e7110e1e83ae
LinaHai1 <Lina.Haigoun@emsi-edu.ma>
2025-05-02 04:37:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 (function () {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var names = ["Jarid", "Adam", "Sofia", "Lina", "Jamal", "Jawad", "Janat", "Khalil", "Sarah", "Mouad", "Jaber", "Salma"];
 
   for (var i = 0; i < names.length; i++) {
     var name = names[i];
